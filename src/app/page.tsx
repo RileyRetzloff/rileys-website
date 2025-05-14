13748ff3a8a7c776deb8c52bdd5ab0c5f237ba97
RileyRetzloff <rirye811@gmail.com>
2025-05-13 22:46:26 -0400

    update site metadata; update styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const fileUrls = [
 export default function HomePage() {
   return (
     <main className="flex h-screen w-screen flex-col place-items-center">
-      <div id="body" className="h-full bg-gray-50 sm:w-full lg:w-4/5 xl:w-2/3">
+      <div id="body" className="h-full bg-gray-50 sm:w-full lg:w-4/5 xl:w-3/4">
         <div
           id="header"
           className="flex place-items-center justify-between bg-gray-100 px-5"
@@ -16,11 +16,11 @@ export default function HomePage() {
           <div className="h-8 w-8 rounded-full bg-gray-900 shadow-black transition-all duration-300 hover:scale-95 hover:shadow-lg/50"></div>
         </div>
         <div id="content" className="px-8 py-5">
-          <h2>this is my website</h2>
-          <p>that&apos;s what it is.</p>
-          <div className="w-72 overflow-hidden rounded-lg">
+          <div className="h-96 overflow-hidden rounded-xl">
             <img src={fileUrls[0]} alt="Image 1" />
           </div>
+          <h2>this is my website</h2>
+          <p>that&apos;s what it is.</p>
         </div>
       </div>
     </main>
