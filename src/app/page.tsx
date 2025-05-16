4f3e4118895a1ab0a5f170292b08f5ef1636707d
RileyRetzloff <rirye811@gmail.com>
2025-05-15 21:13:00 -0400

    more minor content/styling updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,38 @@ export default function HomePage() {
     <main className="flex h-screen w-screen flex-col place-items-center px-3">
       <div>
         <h1>Riley Retzloff</h1>
-        <div className="overflow-hidden rounded-xl sm:h-48 md:h-64 lg:h-72 xl:h-96">
+        <div className="overflow-hidden rounded-xl outline outline-white sm:h-48 md:h-64 lg:h-72 xl:h-96">
           <img src={fileUrls[0]} alt="Image 1" className="object-cover" />
-          <div></div>
         </div>
         <h2>this is my website</h2>
-        <p>that&apos;s just what it is.</p>
+        <h3>that&apos;s just what it is.</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ut
+          assumenda facere rerum eligendi eaque nulla molestias dolore!
+          Laboriosam necessitatibus minima corporis non aut optio voluptates
+          consectetur nisi, molestias placeat.
+        </p>
+        <code>test</code>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos magnam
+          ullam, optio dolore dicta nemo. Aspernatur ea doloremque alias odio
+          tenetur recusandae ad qui expedita, quasi eos animi aperiam
+          perspiciatis voluptate, obcaecati magnam, neque sapiente? Labore eaque
+          aliquam consectetur voluptatibus illo iure similique.
+        </p>
         <p>&macr;&#92;_(&#12484;)_/&macr;</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus amet
+          esse sed nobis quaerat, ex totam, minima deleniti dolorem nihil libero
+          voluptate. Eos voluptatibus libero cupiditate obcaecati laboriosam ad
+          reprehenderit?
+        </p>
+        <h1>heading 1</h1>
+        <h2>heading 2</h2>
+        <h3>heading 3</h3>
+        <h4>heading 4</h4>
+        <h5>heading 5</h5>
+        <h6>heading 6</h6>
       </div>
     </main>
   );
